f4d173cbe99fe1c5806b5ebd8705f74d66b5a603
Sven Marquardt <sven2q@hotmail.de>
2017-10-21 23:55:42 +0200

    init

---
+++ illnes-factorisator/src/main/java/space/smarquardt/illnes_factorisator/App.java
@@ -0,0 +1,13 @@
+package space.smarquardt.illnes_factorisator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
