0a128d2b8f10bcd982d857a4ac6b886de039b667
Sven Marquardt <sven2q@hotmail.de>
2017-10-22 01:04:04 +0200

    Einstellungen laden anhand der Argumente

--- illnes-factorisator/src/main/java/space/smarquardt/illnes_factorisator/App.java
+++ illnes-factorisator/src/main/java/space/smarquardt/illnes_factorisator/App.java
@@ -1,13 +1,27 @@
 package space.smarquardt.illnes_factorisator;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import space.smarquardt.illnes_factorisator.exception.IllnesFactorisatoSettingException;
+import space.smarquardt.illnes_factorisator.settings.Setting;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(final String[] args) throws IllnesFactorisatoSettingException, URISyntaxException
+	{
+		Setting setting;
+		if (ArrayUtils.isEmpty(args)) {
+			// Einstellungen sollten durch das Maven plugin erfolgt sein
+			setting = new Setting();
+		} else {
+			setting = new Setting(new URI(args[0]));
+		}
+	}
 }
