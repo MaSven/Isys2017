d1e97d7e96ff39736427d0307d5f4d8789401b74
Sven Marquardt <sven2q@hotmail.de>
2017-10-25 21:51:04 +0200

    Neue Eigenschaften
    * Kann krank werden
    * Kann gesund werden
    * Hat tage bis zu genesung

--- illnes-factorisator/src/main/java/space/smarquardt/illnes_factorisator/simulation/Attendee.java
+++ illnes-factorisator/src/main/java/space/smarquardt/illnes_factorisator/simulation/Attendee.java
@@ -3,6 +3,8 @@
  */
 package space.smarquardt.illnes_factorisator.simulation;
 
+import java.util.Arrays;
+
 /**
  * Teilnehmer bei der Simulation
  * 
@@ -20,6 +22,18 @@ public class Attendee {
 	 * Welche nummer hat dieser Teilnehmer.
 	 */
 	private int id;
+	/**
+	 * Ist ein Teilnehme krank
+	 */
+	private boolean isIll = false;
+	/**
+	 * Wurde dieser Teilnehmer geimpft
+	 */
+	private boolean isImmun = false;
+	/**
+	 * Tage bis Teilnehmer wieder gesund ist
+	 */
+	private int daysLeftForRecovery;
 
 
 	/**
@@ -55,6 +69,146 @@ public class Attendee {
 	 */
 	public int getId() {
 		return this.id;
+	}
+
+	/**
+	 * @return the isIll
+	 */
+	public boolean isIll() {
+		return this.isIll;
+	}
+
+	/**
+	 * @param isIll
+	 *            the isIll to set
+	 */
+	public void setIll() {
+		this.isIll = true;
+	}
+
+	/**
+	 * @return the isImmun
+	 */
+	public boolean isImmun() {
+		return this.isImmun;
+	}
+
+	/**
+	 * @param isImmun
+	 *            the isImmun to set
+	 */
+	public void setImmun() {
+		this.isImmun = true;
+	}
+
+	/**
+	 * @return the daysLeftForRecovery
+	 */
+	public int getDaysLeftForRecovery() {
+		return this.daysLeftForRecovery;
+	}
+
+	/**
+	 * @param daysLeftForRecovery
+	 *            the daysLeftForRecovery to set
+	 */
+	public void setDaysLeftForRecovery(final int daysLeftForRecovery) {
+		this.daysLeftForRecovery = daysLeftForRecovery;
+	}
+
+	/**
+	 * {@link Attendee} ist nicht mehr Krank
+	 * 
+	 * @author Sven Marquardt
+	 * @since 25.10.2017
+	 */
+	public void setRecoverd() {
+		this.isIll = false;
+	}
+
+	/**
+	 * Dekrementiere tage bis zur genesung um einen. Wenn keine tage mehr übrig ist
+	 * er gesund
+	 * 
+	 * @author Sven Marquardt
+	 * @since 25.10.2017
+	 */
+	public void decreaseDaysLeftForRecovery() {
+
+		if (this.daysLeftForRecovery > 1) {
+			this.daysLeftForRecovery--;
+		} else {
+			this.daysLeftForRecovery--;
+			this.isIll = false;
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(this.chancePerAttendee);
+		result = prime * result + this.daysLeftForRecovery;
+		result = prime * result + this.id;
+		result = prime * result + (this.isIll ? 1231 : 1237);
+		result = prime * result + (this.isImmun ? 1231 : 1237);
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Attendee)) {
+			return false;
+		}
+		final Attendee other = (Attendee) obj;
+		if (!Arrays.equals(this.chancePerAttendee, other.chancePerAttendee)) {
+			return false;
+		}
+		if (this.daysLeftForRecovery != other.daysLeftForRecovery) {
+			return false;
+		}
+		if (this.id != other.id) {
+			return false;
+		}
+		if (this.isIll != other.isIll) {
+			return false;
+		}
+		if (this.isImmun != other.isImmun) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		if (this.isIll) {
+			return "K";
+		} else if (this.isImmun) {
+			return "I";
+		}
+		return "O";
 	}
 
 }
