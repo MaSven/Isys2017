0136cffedac548ae5827227968c81e3f36495b42
Sven Marquardt <sven2qhotmail.de>
2018-01-22 02:22:53 +0100

    Zwei sortieralgorithmen zum preis von einem

--- sort-algorithm/sort-algorithm-implementations/src/main/java/space/smarquardt/sortalgorithm/implementations/impl/OwnSort.java
+++ sort-algorithm/sort-algorithm-implementations/src/main/java/space/smarquardt/sortalgorithm/implementations/impl/OwnSort.java
@@ -16,6 +16,12 @@ public class OwnSort extends AbstractSort {
 
 	private final Queue<Double> valuesToSort;
 
+	private Double valueNotAdded;
+
+	private boolean cycleExceededValueAdd = true;
+
+	private double[] sortedValuesForShortCycle;
+
 	public OwnSort(final int cylces, final double[] data) {
 		super(cylces, data);
 		this.tree = new RedBlackTree(this);
@@ -24,11 +30,24 @@ public class OwnSort extends AbstractSort {
 
 	@Override
 	public double[] getResult() {
+		if (this.sortedValuesForShortCycle != null) {
+			return this.sortedValuesForShortCycle.clone();
+		}
 		final double[] sorted = this.tree.getSorted();
 		final Double[] notSorted = this.valuesToSort.toArray(new Double[this.valuesToSort.size()]);
 		double[] both = new double[0];
 		both = ArrayUtils.insert(0, both, sorted);
-		both = ArrayUtils.insert(sorted.length, both, ArrayUtils.toPrimitive(notSorted));
+
+		if (this.isCycleExceeded() && this.cycleExceededValueAdd) {
+			both = ArrayUtils.insert(sorted.length, both, this.valueNotAdded);
+
+			both = ArrayUtils.insert(sorted.length + 1, both, ArrayUtils.toPrimitive(notSorted));
+
+			this.cycleExceededValueAdd = false;
+		} else {
+			both = ArrayUtils.insert(sorted.length, both, ArrayUtils.toPrimitive(notSorted));
+		}
+
 		return both;
 	}
 
@@ -40,11 +59,33 @@ public class OwnSort extends AbstractSort {
 	@Override
 	public void sort() {
 		this.valuesToSort.addAll(Arrays.stream(this.data).boxed().collect(Collectors.toList()));
+		if (((this.data.length <= 100))) {
+			this.sortedValuesForShortCycle = this.data.clone();
+			this.shortSort();
+			return;
+		}
 		while (!this.valuesToSort.isEmpty()) {
 			final double d = this.valuesToSort.remove();
+			this.valueNotAdded = d;
 			this.tree.insert(d);
 		}
-		System.out.println("sort finished");
+
+	}
+
+	private void shortSort() {
+		final int l = this.sortedValuesForShortCycle.length - 1;
+		final int gaps[] = { 1, 2, 7, 15, 31, 63 };
+		for (final int gap : gaps) {
+			for (int i = gap; i < l; i++) {
+				int j;
+				final double temp = this.sortedValuesForShortCycle[i];
+				for (j = i; (j >= gap)
+						&& (this.isGreaterThan.test(this.sortedValuesForShortCycle[j - gap], temp)); j -= gap) {
+					this.sortedValuesForShortCycle[j] = this.sortedValuesForShortCycle[j - gap];
+				}
+				this.sortedValuesForShortCycle[j] = temp;
+			}
+		}
 	}
 
 }
--- sort-algorithm/sort-algorithm-tautest/src/main/java/org/sort/algorithm/tautest/TestSortAlg.java
+++ sort-algorithm/sort-algorithm-tautest/src/main/java/org/sort/algorithm/tautest/TestSortAlg.java
@@ -1,9 +1,15 @@
 package org.sort.algorithm.tautest;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.math3.stat.correlation.KendallsCorrelation;
 import org.randomarraygenerator.RandomArrayGenerator;
+
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.MultimapBuilder;
+import com.google.common.io.Files;
 
 import space.smarquardt.sortalgorithm.implementations.RangedSort;
 import space.smarquardt.sortalgorithm.implementations.impl.Mergesort;
@@ -14,31 +20,76 @@ public class TestSortAlg {
 
 	public static void main(final String[] args) {
 		final int count = 300;
-		final int[] lengthOfArray = { 100, 120, 140, 160 };
+		final int[] lengthOfArray = { 100, 120, 140, 160, 200 };
+		final ListMultimap<Integer, Double> resultsQuickSort = MultimapBuilder.hashKeys().arrayListValues().build();
+		final ListMultimap<Integer, Double> resultsMergeSort = MultimapBuilder.hashKeys().arrayListValues().build();
+		final ListMultimap<Integer, Double> resultsOwnSort = MultimapBuilder.hashKeys().arrayListValues().build();
 		RangedSort sort;
-		for (final int length : lengthOfArray) {
-			final double[] testArray = RandomArrayGenerator.generateArray(length);
-			sort = new Quicksort(count, testArray);
-			TestSortAlg.sortAndPrintResult(length, testArray, sort);
-			sort = new Mergesort(count, testArray);
-			TestSortAlg.sortAndPrintResult(length, testArray, sort);
-			sort = new OwnSort(count, testArray);
-			TestSortAlg.sortAndPrintResult(length, testArray, sort);
-
+		for (int i = 1000; i > 0; i--) {
+			for (final int length : lengthOfArray) {
+				final double[] testArray = RandomArrayGenerator.generateArray(length);
+				double result;
+				sort = new Quicksort(count, testArray);
+				result = TestSortAlg.sortAndPrintResult(length, testArray, sort);
+				resultsQuickSort.put(length, result);
+				sort = new Mergesort(count, testArray);
+				result = TestSortAlg.sortAndPrintResult(length, testArray, sort);
+				resultsMergeSort.put(length, result);
+				sort = new OwnSort(count, testArray);
+				result = TestSortAlg.sortAndPrintResult(length, testArray, sort);
+				resultsOwnSort.put(length, result);
+			}
 		}
+
+		// final Percentile percentile = new Percentile();
+		// for (final int length : lengthOfArray) {
+		// double[] results = ArrayUtils
+		// .toPrimitive(resultsQuickSort.get(length).toArray(new
+		// Double[resultsQuickSort.get(length).size()]));
+		// System.out.println("Ergebnis Quicksort: " + percentile.evaluate(results,
+		// 10.0));
+		// results = ArrayUtils
+		// .toPrimitive(resultsMergeSort.get(length).toArray(new
+		// Double[resultsMergeSort.get(length).size()]));
+		// System.out.println("Ergebnis MergeSort: " + percentile.evaluate(results,
+		// 10.0));
+		// results = ArrayUtils
+		// .toPrimitive(resultsOwnSort.get(length).toArray(new
+		// Double[resultsOwnSort.get(length).size()]));
+		// System.out.println("Ergebnis OwnSort: " + percentile.evaluate(results,
+		// 10.0));
+		// }
+		final StringBuilder builder = new StringBuilder();
+		for (final Integer key : resultsOwnSort.keySet()) {
+			builder.append(key).append("\n");
+			final List<Double> results = resultsOwnSort.get(key);
+			results.sort(Double::compareTo);
+			results.forEach(v -> builder.append(v).append("\n"));
+			try {
+				Files.write(builder.toString().getBytes(), new File("OwnSort" + key + "csv"));
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			builder.delete(0, builder.length());
+		}
+
 	}
 
 	/**
+	 *
 	 * @param length
 	 * @param testArray
 	 * @param sort
 	 */
-	private static void sortAndPrintResult(final int length, final double[] testArray, final RangedSort sort) {
+	private static double sortAndPrintResult(final int length, final double[] testArray, final RangedSort sort) {
 		sort.sort();
 		final double[] resultArray = sort.getSortedArrayTillRange();
-		System.out.println("Tau mit " + length + " länge und " + sort.nameOfAlgorithm() + " ergibt: "
-				+ new KendallsCorrelation().correlation(resultArray, testArray) + " länge des test array: "
-				+ resultArray.length + " und inhalt " + Arrays.toString(resultArray));
+		final double result = new KendallsCorrelation().correlation(resultArray, testArray);
+		// System.out.println("Tau mit " + length + " länge und " +
+		// sort.nameOfAlgorithm() + " ergibt: " + result
+		// + " länge des test array: " + resultArray.length + " und inhalt " +
+		// Arrays.toString(resultArray));
+		return result;
 	}
 
 }
